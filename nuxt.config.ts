40776dc62b3e6f085e2aab9729cbd3bddf5cba01
Konkamon Sion <bigkonkamon@gmail.com>
2025-06-22 14:16:50 +0700

    feat: :sparkles: Use 'nuxt/fonts' instead of 'nuxtjs/google-fonts'

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
     '@nuxt/eslint',
     '@nuxt/image',
     '@nuxtjs/i18n',
-    '@nuxtjs/google-fonts',
+    '@nuxt/fonts',
     '@nuxt/content',
     'motion-v/nuxt',
   ],
@@ -50,17 +50,19 @@ export default defineNuxtConfig({
       },
     },
   },
-  googleFonts: {
-    families: {
-      'Nunito Sans': {
-        wght: [400, 500, 600, 700, 800],
+  fonts: {
+    provider: 'google',
+    families: [
+      {
+        name: 'Nunito Sans', display: 'swap', global: true, subsets: ['latin', 'latin-ext'], styles: ['normal'],
       },
-      'Noto Sans Thai': {
-        wght: [400, 600, 700, 800],
+      {
+        name: 'Noto Sans Thai', display: 'swap', global: true, subsets: ['thai'], styles: ['normal'],
       },
-      'Fira Code': {
-        wght: [400, 600, 700, 800],
-      },
+    ],
+    defaults: {
+      weights: [300, 400, 500, 700, 800],
+      styles: ['normal'],
     },
   },
   i18n: {
