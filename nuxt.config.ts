3771c0809eec2020204e178c2a8f01de8997ffb9
Konkamon Sion <bigkonkamon@gmail.com>
2025-06-28 15:02:53 +0700

    feat(i18n): :mag: 'prefix' strat & isCatchallLocale for English

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -69,7 +69,7 @@ export default defineNuxtConfig({
       optimizeTranslationDirective: false,
     },
     baseUrl: 'https://www.bkozii.com',
-    strategy: 'prefix_and_default',
+    strategy: 'prefix',
     defaultLocale: 'en',
     lazy: true,
     skipSettingLocaleOnNavigate: true,
@@ -86,6 +86,7 @@ export default defineNuxtConfig({
         language: 'en-US',
         name: 'English',
         file: 'en.json',
+        isCatchallLocale: true,
       },
     ],
     detectBrowserLanguage: {
