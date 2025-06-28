fb7fb7b5fb24c5a9291742e907ae7039b2258141
Konkamon Sion <bigkonkamon@gmail.com>
2025-06-28 20:31:11 +0700

    feat(navbar): :sparkles: Improve MainNavbar / add MobileNav component
    - Removed Home link
    - Add Mobile Navigation Menu using USlideover
    - Move UNav to left side of Navbar
    - Add spacing between right-side buttons
    - Improve Icons for UNav items

--- app/components/MainNavbar.vue
+++ app/components/MainNavbar.vue
@@ -1,29 +1,39 @@
 <template>
   <div
-    class="top-0 z-10 sticky bg-default/80 drop-shadow-sm backdrop-blur-xl border-default border-b"
+    class="top-0 z-10 sticky bg-default border-default border-b"
   >
-    <UContainer class="items-center border-default border-x w-full">
+    <UContainer class="border-default border-x w-full">
       <header class="flex items-center h-(--header-height)">
-        <div class="flex-1 font-mono tracking-tighter">
-          <ULink
+        <div class="flex flex-row flex-1 items-center space-x-4">
+          <NuxtLinkLocale
             id="myName"
             to="/"
-            class="flex items-center text-highlighted"
+            active-class="decoration-primary decoration-2 underline underline-offset-[12px]"
+            class="font-mono font-bold text-highlighted"
             aria-label="logo"
           >
-            <h1 class="font-bold text-lg">bkozii<span class="font-normal text-sm">.com</span></h1>
-          </ULink>
-        </div>
-        <div class="flex flex-1 justify-center">
+            <h1>
+              bkozii<span class="font-normal text-sm">.com</span>
+            </h1>
+          </NuxtLinkLocale>
           <UNavigationMenu
+            class="hidden sm:block"
+            :ui="{ link: 'after:-bottom-1 after:h-0.5' }"
             :items="navItems"
             variant="link"
             highlight
           />
         </div>
-        <div class="flex flex-row flex-1 justify-end shrink-0">
+        <div class="flex flex-row justify-end space-x-2 shrink-0">
           <LazyNavbarLangSwitch />
           <LazyNavbarThemeSwitch />
+          <LazyUButton
+            variant="ghost"
+            class="sm:hidden inline-flex"
+            icon="solar:hamburger-menu-linear"
+            color="neutral"
+            @click="openModal"
+          />
         </div>
       </header>
     </UContainer>
@@ -32,34 +42,36 @@
 
 <script lang="ts" setup>
 import type { NavigationMenuItem } from '@nuxt/ui'
+import { LazyNavbarMobileNav } from '#components'
 
 const localePath = useLocalePath()
+const overlay = useOverlay()
 
 const navItems = computed<NavigationMenuItem[]>(() => [
   {
-    label: 'Home',
-    to: localePath('/'),
-    icon: 'solar:home-2-linear',
-  },
-  {
     label: 'Projects',
     to: localePath('/projects'),
-    icon: 'ph:folder-open-duotone',
+    icon: 'solar:inbox-archive-line-duotone',
   },
   {
     label: 'What\'s In My Bag?',
-    icon: 'solar:backpack-linear',
+    icon: 'solar:backpack-line-duotone',
     disabled: true,
   },
-  /*   {
-    label: 'Bookmarks',
-    to: '/Bookmarks',
-    icon: 'ph:bookmark-duotone',
-  }, */
   {
     label: 'Blog',
-    icon: 'solar:book-2-linear',
+    icon: 'solar:notebook-line-duotone',
     disabled: true,
   },
 ])
+
+const modal = overlay.create(LazyNavbarMobileNav, {
+  props: {
+    menu: navItems.value,
+  },
+})
+
+async function openModal() {
+  modal.open()
+}
 </script>
