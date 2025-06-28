1748a761288d5df9a8567921f9ef5ccbf5fca043
Konkamon Sion <bigkonkamon@gmail.com>
2025-06-28 20:25:35 +0700

    feat: :lipstick: Soften Alert / Add missing closing parentheses

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -4,12 +4,13 @@
     <UAlert
       description="You are visiting my new portfolio design! This is a work in progress and most of the content is not yet available."
       class="py-1 rounded-none text-center"
+      variant="soft"
       color="primary"
       :ui="{ root: 'z-20' }"
     />
     <MainNavbar />
     <UContainer class="space-y-8 border-default md:border-x">
-      <main class="pb-12 min-h-[calc(100dvh-(var(--header-height))]">
+      <main class="pb-12 min-h-[calc(100dvh-(var(--header-height)))]">
         <slot />
       </main>
     </UContainer>
