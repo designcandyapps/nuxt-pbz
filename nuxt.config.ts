f7ef966d93d478029e075f1f55eb9ac1a484fe99
Erin Dunn Halligan <erin@designcandy.com>
2025-09-25 07:18:34 -0500

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,11 +18,7 @@ export default defineNuxtConfig({
   app: {
     head:{
       link:[
-        //{rel:'stylesheet',href:'/st.css'},
-        {rel:'stylesheet',href:'/fo.css'},
-        //{rel:'stylesheet',href:'/sc.css'},
-        //{rel:'stylesheet',href:'/sl.css'},
-        //{rel:'stylesheet',href:'/ct.css'},
+        {rel:'stylesheet',href:'/fo.css'},  //{rel:'stylesheet',href:'/sl.css'},{rel:'stylesheet',href:'/ct.css'},
       ],
       script:[
         {src:'/jq.js'},
