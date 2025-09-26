b8b88ac85340cae030dee7fb7832274a85e42c13
Erin Dunn Halligan <erin@designcandy.com>
2025-09-26 11:22:34 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -22,7 +22,18 @@ onMounted(()=>{
 </script>
 
 <template>
-Blah
+  <main>
+    <img id="ee" src="https://pinfluents.com/_BCK/4/im/bp.png" width="60" height="60">
+    <div class="g">
+      <div id="t" style="border:3px solid red;"></div>
+      <div id="y" style="border:3px solid green;">s</div>
+      <div id="z" style="border:3px solid blue;"><ColorThief /></div>
+      <input id="pr" name="pr" value="hats">
+      <input id="prompt" v-model="prompt">
+      <input id="pho" v-model="pho"><input id="pho2" v-model="pho2">
+      <div id="response" v-if="response">{{response}}</div>
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
