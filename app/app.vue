856359bc9009ae71480a14411f51f56fb0eab37c
Erin Dunn Halligan <erin@designcandy.com>
2025-09-29 13:40:29 -0500

    Update app.vue

--- app/app.vue
+++ app/app.vue
@@ -11,18 +11,38 @@ useHead({
   meta: [{name:'author',content:'Konkamon Sion'},{name:'viewport',content:'width=device-width,initial-scale=1'},{name:'charset',content:'utf-8'},{name:'theme-color',content:'#3892a2'}],
 })
 useSeoMeta({ogLocale:langCode,themeColor:'#3892a2',charset:'utf-8',ogType:'website'})
+
+/*import type {ParsedContent} from '@nuxt/content'
+const {seo}=useAppConfig()
+const {data:navigation}=await useAsyncData('navigation',()=>fetchContentNavigation())
+const {data:files}=useLazyFetch<ParsedContent[]>('/api/search.json',{default:()=>[],server:false})
+useHead({meta:[{name:'viewport',content:'width=device-width,initial-scale=1'}],htmlAttrs:{lang:'en'}})
+provide('navigation',navigation)*/
 </script>
 
-<template>
-  <NuxtLoadingIndicator color="#3892a2" />
+<!--template>
+  <NuxtLoadingIndicator />
   <UApp :tooltip="{delayDuration:0}" :locale="locales[locale]">
     <NuxtLayout>
-      <NuxtPage keepalive />
+      <NuxtPage />
     </NuxtLayout>
   </UApp>
+</template-->
+
+<template>
+  <div>
+    <NuxtLoadingIndicator />
+    <AppHeader />
+    <UMain>
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+    </UMain>
+    <AppFooter />
+  </div>
 </template>
 
-<style>
+<!--style>
 @media (min-width: 400px) and (prefers-reduced-motion: no-preference) {
   .page-enter-active {
     transition: all 0.5s cubic-bezier(0.25, 1, 0.25, 1);
@@ -36,4 +56,4 @@ useSeoMeta({ogLocale:langCode,themeColor:'#3892a2',charset:'utf-8',ogType:'websi
     transform: translateY(-10px);
   }
 }
-</style>
+</style-->
