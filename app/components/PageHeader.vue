4d5942f0be4a642d16947ed0ef76cd02c2cf6f5e
Erin Dunn Halligan <erin@designcandy.com>
2025-09-29 17:10:10 -0500

    Update PageHeader.vue

--- app/components/PageHeader.vue
+++ app/components/PageHeader.vue
@@ -1,19 +1,35 @@
+<script setup lang="ts">
+import type {NavItem} from '@nuxt/content'
+const navigation=inject<NavItem[]>('navigation',[])
+const {header}=useAppConfig()
+</script>
+
 <template>
+  <UHeader>
+    <template #logo>
+      <template v-if="header?.logo?.dark||header?.logo?.light"><UColorModeImage v-bind="{ class:'h-6 w-auto',...header?.logo }" /></template>
+      <template v-else>
+        <img id="ee" src="https://pinfluents.com/_BCK/4/im/bp1.png" width="60" height="60">
+        <!--NuxtImg id="nu" src="/public/dc2.png" width="60" height="60" format="png" @load="getCP" /-->
+      </template>
+    </template>
+    <template v-if="header?.search" #center>
+      <UContentSearchButton class="hidden lg:flex" />
+    </template>
+    <template #right>
+      <UContentSearchButton v-if="header?.search" :label="null" class="lg:hidden" />
+      <template v-if="header?.links"><UButton v-for="(link,index) of header.links" :key="index" v-bind="{ color:'gray',variant:'ghost',...link }" /></template>
+    </template>
+    <template #panel><UNavigationTree :links="mapContentNavigation(navigation)" /></template>
+  </UHeader>
+</template>
+
+<!--template>
   <div>
-    <section
-      class="dark:prose-invert prose-h1:m-0 py-8 sm:py-12 max-w-none lg:text-left text-center prose prose-sm md:prose-base"
-    >
+    <section class="dark:prose-invert prose-h1:m-0 py-8 sm:py-12 max-w-none lg:text-left text-center prose prose-sm md:prose-base">
       <h1>{{ $props.title }}</h1>
-      <p class="text-balance">
-        {{ $props.description }}
-      </p>
+      <p class="text-balance">{{ $props.description }}</p>
     </section>
   </div>
 </template>
-
-<script lang="ts" setup>
-defineProps({
-  title: String,
-  description: String,
-})
-</script>
+<script lang="ts" setup>defineProps({title:String,description:String})</script-->
