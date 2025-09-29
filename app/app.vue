260e733389144310727c49cf5985cffee6a627ca
Erin Dunn Halligan <erin@designcandy.com>
2025-09-29 18:21:27 -0500

    Update app.vue

--- app/app.vue
+++ app/app.vue
@@ -4,11 +4,10 @@ const locales={en,th}
 const {locale,localeProperties}=useI18n()
 const lang=computed(()=>locales[locale.value].code)
 const langCode=computed(()=>localeProperties.value.language)
-//useHead({htmlAttrs:{lang}, titleTemplate:(titleChunk)=>{return titleChunk?`${titleChunk} - Konkamon Sion` : 'Konkamon Sion'}, link:[{rel:'icon',type:'image/vnd.microsoft.icon',href:'/favicon.ico'}], meta:[{name:'author',content:'Konkamon Sion'},{name:'viewport',content:'width=device-width,initial-scale=1'},{name:'charset',content:'utf-8'},{name:'theme-color',content:'#3892a2'}]})
-//useSeoMeta({ogLocale:langCode,themeColor:'#3892a2',charset:'utf-8',ogType:'website'})
+useHead({htmlAttrs:{lang}, titleTemplate:(titleChunk)=>{return titleChunk?`${titleChunk} - Konkamon Sion` : 'Konkamon Sion'}, link:[{rel:'icon',type:'image/vnd.microsoft.icon',href:'/favicon.ico'}], meta:[{name:'author',content:'Konkamon Sion'},{name:'viewport',content:'width=device-width,initial-scale=1'},{name:'charset',content:'utf-8'},{name:'theme-color',content:'#3892a2'}]})
+useSeoMeta({ogLocale:langCode,themeColor:'#3892a2',charset:'utf-8',ogType:'website'})
 
-/*
-import type {ParsedContent} from '@nuxt/content'
+/*import type {ParsedContent} from '@nuxt/content'
 const {seo}=useAppConfig()
 //const {data:navigation}=await useAsyncData('navigation',()=>fetchContentNavigation())
 //const {data:files}=useLazyFetch<ParsedContent[]>('/api/search.json',{default:()=>[],server:false})
@@ -19,14 +18,21 @@ useHead({meta:[{name:'viewport',content:'width=device-width,initial-scale=1'}],h
 
 <template>
   <NuxtLoadingIndicator />
-  <AppHeader />
+
+  <UApp :tooltip="{delayDuration:0}" :locale="locales[locale]">
+    <NuxtLayout>
+      <NuxtPage keepalive />
+    </NuxtLayout>
+  </UApp>
+
+  <!--AppHeader />
   <UApp>
-    <!--UMain-->
+    <UMain>
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
-    <!--/UMain-->
+    </UMain>
   </UApp>
-  <AppFooter />
+  <AppFooter /-->
   <!--ClientOnly><LazyUContentSearch :files="files" :navigation="navigation" /></ClientOnly><UNotifications /-->
 </template>
