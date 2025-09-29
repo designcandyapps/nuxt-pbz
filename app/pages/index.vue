3e58d0f6395d0049c4f1f67d90a2ebca6c33a84d
Erin Dunn Halligan <erin@designcandy.com>
2025-09-29 14:05:15 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -38,22 +38,6 @@ onMounted(()=>{
   </main>
 </template>
 
-<!--template>
-  <div>
-    <!--ULandingHero v-if="page.hero" v-bind="page.hero"-->
-      <div class="g">
-        <div id="t" style="border:3px solid red;"></div>
-        <div id="y" style="border:3px solid green;">s</div>
-        <div id="z"><CT /></div>
-        <input id="pr" name="pr" value="Open Source">
-        <input id="prompt" v-model="prompt">
-        <input id="pho" v-model="pho"><input id="pho2" v-model="pho2">
-        <div id="response" v-if="response">{{response}}</div>
-      </div>
-    <!--/ULandingHero-->
-  </div>
-</template-->
-
 <script lang="ts">
 export default{
   data(){return{pr:"",response:null}},
