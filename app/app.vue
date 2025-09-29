82bf85c75e41e2f5adc1dbfe0455dd5acd9cfeaa
Erin Dunn Halligan <erin@designcandy.com>
2025-09-29 17:11:20 -0500

    Update app.vue

--- app/app.vue
+++ app/app.vue
@@ -6,25 +6,23 @@ const lang=computed(()=>locales[locale.value].code)
 const langCode=computed(()=>localeProperties.value.language)*/
 //useHead({htmlAttrs:{lang}, titleTemplate:(titleChunk)=>{return titleChunk?`${titleChunk} - Konkamon Sion` : 'Konkamon Sion'}, link:[{rel:'icon',type:'image/vnd.microsoft.icon',href:'/favicon.ico'}], meta:[{name:'author',content:'Konkamon Sion'},{name:'viewport',content:'width=device-width,initial-scale=1'},{name:'charset',content:'utf-8'},{name:'theme-color',content:'#3892a2'}]})
 //useSeoMeta({ogLocale:langCode,themeColor:'#3892a2',charset:'utf-8',ogType:'website'})
+
 import type {ParsedContent} from '@nuxt/content'
 const {seo}=useAppConfig()
-const {data:navigation}=await useAsyncData('navigation',()=>fetchContentNavigation())
-const {data:files}=useLazyFetch<ParsedContent[]>('/api/search.json',{default:()=>[],server:false})
-
+//const {data:navigation}=await useAsyncData('navigation',()=>fetchContentNavigation())
+//const {data:files}=useLazyFetch<ParsedContent[]>('/api/search.json',{default:()=>[],server:false})
 useHead({meta:[{name:'viewport',content:'width=device-width,initial-scale=1'}],htmlAttrs:{lang:'en'}})
 //provide('navigation',navigation)
 </script>
 
 <template>
   <NuxtLoadingIndicator />
-  <UApp :tooltip="{delayDuration:0}" :locale="locales[locale]">
   <!--AppHeader /-->
-  <!--UApp><UMain>
+  <UApp><UMain>
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
-  </UMain></UApp-->
-  <!--AppFooter />
-  <ClientOnly><LazyUContentSearch :files="files" :navigation="navigation" /></ClientOnly>
-  <UNotifications /-->
+  </UMain></UApp>
+  <!--AppFooter /-->
+  <!--ClientOnly><LazyUContentSearch :files="files" :navigation="navigation" /></ClientOnly><UNotifications /-->
 </template>
