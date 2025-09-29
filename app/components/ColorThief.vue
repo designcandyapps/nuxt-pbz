921146fec8bc1707ec493c8f03ab8445cc999117
Erin Dunn Halligan <erin@designcandy.com>
2025-09-29 07:11:35 -0500

    Update ColorThief.vue

--- app/components/ColorThief.vue
+++ app/components/ColorThief.vue
@@ -6,10 +6,17 @@ const generatePalette=async()=>{
   isLoading.value=true; proxyUrl.value=`/api/proxy?url=${encodeURIComponent(imageUrl.value)}`;
   //alert("PR-URL: "+proxyUrl.value);
   const img=new Image(); img.crossOrigin="Anonymous"; img.src=proxyUrl.value;
+  alert("IMG-SRC: "+img.src);
+
   img.onload=()=>{
+    alert("IMG: "+img);
+
     const colorThief=new ColorThief(); let colors=colorThief.getPalette(img).map((c)=>toLCH({r:c[0]/255,g:c[1]/255,b:c[2]/255,mode:"rgb"}));
-    const palettesz=discoverPalettes(colors); document.getElementById("y").innerHTML=`<span class="content"></span>`;
-    //alert("Z1: "+document.getElementById("z").innerHTML); 
+    const palettesz=discoverPalettes(colors);
+
+    document.getElementById("y").innerHTML=`<span class="content"></span>`;
+    //alert("Z1: "+document.getElementById("z").innerHTML);
+
     var i=0; for(const type of Object.keys(palettesz)){
       const paletteWrapper=document.createElement("span"); paletteWrapper.classList.add("palette-colors"); document.querySelector(".content").appendChild(paletteWrapper);
       paletteWrapper.innerHTML=palettesz[type].colors.reduce((html,color)=>{i++; html+=`<span id="dv${i}" style="background:${formatHex(color)}"></span>`;return html},"");
