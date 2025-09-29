9b75f9cdd0e96f0f564e8ff07903540f818252ee
Erin Dunn Halligan <erin@designcandy.com>
2025-09-29 18:19:37 -0500

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,13 +30,13 @@ export default defineNuxtConfig({
   devtools: {enabled:true},
   typescript: {strict:false},
   future: {compatibilityVersion:4},
-  experimental: {noVueServer:false}, //XXOO
+  //experimental: {noVueServer:false}, //XXOO
 
   compatibilityDate: '2025-06-03',
   nitro: {prerender:{autoSubfolderIndex:false,crawlLinks:true,routes:['/'],ignore:['/__nuxt_content']}},
   //nitro: {prerender:{routes:['/'],crawlLinks:true}},
   routeRules: {'/api/search.json':{prerender:true}},
-  vite: {vue:{features:{optionsAPI:false}}},
+  //vite: {vue:{features:{optionsAPI:false}}},
 
   eslint: {config:{stylistic:{jsx:false,braceStyle:'1tbs',arrowParens:true}}},
   //eslint: {config:{stylistic:{commaDangle:'never',braceStyle:'1tbs'}}
