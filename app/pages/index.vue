0af3273b8ebb1a1111997f89156f43c3b96cb14e
Erin Dunn Halligan <erin@designcandy.com>
2025-09-29 20:25:49 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -22,18 +22,16 @@ onMounted(()=>{
 </script>
 
 <template>
-  <main>
-    <img id="ee" src="https://pinfluents.com/_BCK/4/im/bp.png" width="60" height="60">
-    <div class="g">
-      <div id="t" style="border:3px solid red;"></div>
-      <div id="y" style="border:3px solid green;">s</div>
-      <div id="z" style="border:3px solid blue;"><ColorThief /></div>
-      <input id="pr" name="pr" value="Open Source">
-      <input id="prompt" v-model="prompt">
-      <input id="pho" v-model="pho"><input id="pho2" v-model="pho2">
-      <div id="response" v-if="response">{{response}}</div>
-    </div>
-  </main>
+  <img id="ee" src="https://pinfluents.com/_BCK/4/im/bp.png" width="60" height="60">
+  <div class="g">
+    <div id="t" style="border:3px solid red;"></div>
+    <div id="y" style="border:3px solid green;">s</div>
+    <div id="z" style="border:3px solid blue;"><ColorThief /></div>
+    <input id="pr" name="pr" value="Open Source">
+    <input id="prompt" v-model="prompt">
+    <input id="pho" v-model="pho"><input id="pho2" v-model="pho2">
+    <div id="response" v-if="response">{{response}}</div>
+  </div>
 </template>
 
 <script lang="ts">
