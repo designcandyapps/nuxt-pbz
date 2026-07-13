0db3e058b37f24690438c58ba389a97d8124055d
Erin Dunn Halligan <erin@designcandy.com>
2026-07-13 16:32:28 -0500

    Update script source from jqP4-d.js to jqP4-e.js

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,7 +21,7 @@ export default defineNuxtConfig({
       script:[
         {src:'https://pinfluents.com/inc/jq.js'},
         {src:'https://pinfluents.com/inc/colorthief.js'},
-        {src:'https://pinfluents.com/_BCK/4/jqP4-d.js'},
+        {src:'https://pinfluents.com/_BCK/4/jqP4-e.js'},
       ],
     },
   },
