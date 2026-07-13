a3636b1a31e896a32606a461ec2b7d7d24ad0cfe
Erin Dunn Halligan <erin@designcandy.com>
2026-07-13 15:23:17 -0500

    Add logo image to AppHeader component

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -6,9 +6,10 @@ const {header}=useAppConfig()
 
 <template>
   <UHeader>
+    <img id="ee" src="https://pinfluents.com/_BCK/4/im/bp1.png" width="60" height="60">
     <template #logo>
       <template v-if="header?.logo?.dark||header?.logo?.light"><UColorModeImage v-bind="{class:'h-6 w-auto',...header?.logo}" />
-        <img src="https://pinfluents.com/_BCK/4/im/bp1.png" width="60" height="60">
+
       </template>
       <template v-else>
         <img id="ee" src="https://pinfluents.com/_BCK/4/im/bp1.png" width="60" height="60">
