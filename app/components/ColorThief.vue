0447168ee10524229263ad57fbb60b7347177960
Erin Dunn Halligan <erin@designcandy.com>
2026-08-13 16:17:14 -0500

    Insert image in header and modify image URL logic
    Add an image element to the header and adjust image URL handling.

--- app/components/ColorThief.vue
+++ app/components/ColorThief.vue
@@ -37,14 +37,18 @@ function isColorEqual(c1,c2){return c1.h===c2.h&&c1.l===c2.l&&c1.c===c2.c}
 onMounted(()=>{
   //alert("URL: "+u);
   //alert("0: "+document.getElementById("y").innerHTML);
+
+  alert(document.querySelector("header"));
+  document.querySelector("header").prepend('<img id="ee" src="'+b+'"lo.png" width="60" height="60">');
+
   alert("0: "+document.getElementById("ee"));
   //window.onload=function(){
     alert("1: "+document.getElementById("ee"));
     setTimeout(function(){
       alert("1a: "+document.getElementById("ee"));
       //imageUrl.value=document.getElementById("i1").firstChild.src;
-      imageUrl.value=document.getElementById("ee").src;
-      alert("IM: "+imageUrl.value);
+      //imageUrl.value=document.getElementById("ee").src;
+      //alert("IM: "+imageUrl.value);
       generatePalette();
     },1800);
   //}
