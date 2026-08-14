e8b7f624da45188fb82e8a16278a735cdbd87f66
Erin Dunn Halligan <erin@designcandy.com>
2026-08-13 23:27:57 -0500

    Refactor index.vue layout and remove comments

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -23,18 +23,11 @@ onMounted(()=>{
 </script>
 
 <template>
-  <!--main-->
-    <!--img id="ee" src="https://pinfluents.com/_BCK/4/im/gr.png" width="60" height="60"-->
-    <div class="g" style="position:absolute; top:300; left:99; border:9px solid red;">
-      <div id="t" style="border:3px solid red;"></div>
-      <div id="y" style="border:3px solid green;">s</div>
-      <div id="z" style="border:3px solid blue;"><ColorThief /></div>
-      <input id="pr" name="pr" value="Open Source">
-      <input id="prompt" v-model="prompt">
-      <input id="pho" v-model="pho"><input id="pho2" v-model="pho2">
-      <div id="response" v-if="response">{{response}}</div>
-    </div>
-  <!--/main-->
+  <div class="g" style="position:absolute; top:399; left:99; border:0px solid red;">
+    <div id="t" style="border:0px solid red;"></div><div id="y" style="border:0px solid green;">s</div>
+    <div id="z" style="border:0px solid blue;"><ColorThief /></div>
+    <input id="pr" name="pr" value="Open Source"><input id="prompt" v-model="prompt"><input id="pho" v-model="pho"><input id="pho2" v-model="pho2"><div id="response" v-if="response">{{response}}</div>
+  </div>
 </template>
 
 <script lang="ts">
