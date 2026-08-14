5aaa90cbe280264929fb1b371c93681a4b076e0e
Erin Dunn Halligan <erin@designcandy.com>
2026-08-13 22:42:20 -0500

    Update ColorThief.vue

--- app/components/ColorThief.vue
+++ app/components/ColorThief.vue
@@ -1,13 +1,12 @@
 <script setup>
-import {ref,onMounted} from "vue";
-//import ColorThief from "colorthief";
+import {ref,onMounted} from "vue"; //import ColorThief from "colorthief";
 import {converter,differenceEuclidean,formatHex,nearest} from "culori";
 const imageUrl=ref(""); const proxyUrl=ref(""); const palette=ref([]); const backgroundImage=ref(""); const toLCH=converter("lch"); const isLoading=ref(false);
 
 const generatePalette=async()=>{
-  alert("1c: "+document.getElementById("ee"));
-  imageUrl.value=document.getElementById("ee").src;
-  alert("IU1: "+imageUrl.value);
+  //alert("1c: "+document.getElementById("ee"));
+  //imageUrl.value=document.getElementById("ee").src;
+  //alert("IU1: "+imageUrl.value);
 
   isLoading.value=true; proxyUrl.value=`/api/proxy?url=${encodeURIComponent(imageUrl.value)}`;
   alert("U: "+proxyUrl.value);
@@ -37,17 +36,8 @@ function createScientificPalettes(baseColor){const targetHueSteps={analogous:[0,
 function discoverPalettes(colors){const palettes={}; for(const color of colors){const targetPalettes=createScientificPalettes(color); for(const paletteType of Object.keys(targetPalettes)){const palette=[]; for(const targetColor of targetPalettes[paletteType]){const availableColors=colors.filter((c)=>!palette.some((existing)=>isColorEqual(c,existing))); const match=nearest(availableColors,differenceEuclidean("lch"))(targetColor)[0]; palette.push(match)} palettes[paletteType]={colors:palette}}} return palettes}
 function isColorEqual(c1,c2){return c1.h===c2.h&&c1.l===c2.l&&c1.c===c2.c}
 
-//var u=new URLSearchParams(location.search).get("u"); u=!u||u=="null"?$("#ui").val():"https://www."+u; var s=new URLSearchParams(location.search).get("s"); var t=new URLSearchParams(location.search).get("t");
 onMounted(()=>{
-  //alert("URL: "+u);
-  //alert("0: "+document.getElementById("y").innerHTML);
-
-  //document.querySelector("header").prepend('<img id="ee" src="'+u+'lo.png" width="60" height="60">');
-  //alert("HD: "+document.querySelector("header"));
-
-  //alert("0: "+document.getElementById("ee"));
   //window.onload=function(){
-    //alert("1: "+document.getElementById("ee"));
     setTimeout(function(){
       alert("1a: "+document.getElementById("ee"));
       //imageUrl.value=document.getElementById("i1").firstChild.src;
